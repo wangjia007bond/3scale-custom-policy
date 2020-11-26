ce9481ffaac08acf4a1fb477e0e354be7713caf5
Marcelo Ohashi <mohashi@mohashi-pro.local>
2020-11-26 18:28:00 -0300

    Fixing code

--- policies/guid/0.1/guid.lua
+++ policies/guid/0.1/guid.lua
@@ -3,15 +3,6 @@ local _M = policy.new('GUID policy', '0.1')
 
 local new = _M.new
 
-local function init_config(config)
-  local res = config or { header_name = "GUID" }
-  return res
-end
-
-local function set_request_header(header_name, value)
-  ngx.req.set_header(header_name, value)
-end
-
 function _M.new(config)
   local self = new(config)
   self.config = init_config(config)
@@ -20,6 +11,15 @@ end
 
 function _M:rewrite()
   set_request_header(self.config.header_name, "3f596908-6822-402a-bf3c-e1679f188d56")
+end
+
+local function init_config(config)
+  local res = config or { header_name = "GUID" }
+  return res
+end
+
+local function set_request_header(header_name, value)
+  ngx.req.set_header(header_name, value)
 end
 
 return _M
