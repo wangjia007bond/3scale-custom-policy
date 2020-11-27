f61e3e91a8be909764685f8930db259503773831
Marcelo Ohashi <mohashi@mohashi-pro.local>
2020-11-27 17:09:20 -0300

    Fixing bug in the policy

--- policies/guid/0.1/guid.lua
+++ policies/guid/0.1/guid.lua
@@ -14,11 +14,7 @@ end
 function _M:rewrite()
   local header_name = self.header_name or 'GUID'
 
-  set_request_header(header_name, '3f596908-6822-402a-bf3c-e1679f188d56')
-end
-
-local function set_request_header(header_name, value)
-  ngx.req.set_header(header_name, value)
+  ngx.req.set_header(header_name, '3f596908-6822-402a-bf3c-e1679f188d56')
 end
 
 return _M
