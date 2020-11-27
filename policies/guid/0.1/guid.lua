eabcf60d07ad82822e27d5d6e449b8640882f62d
Marcelo Ohashi <mohashi@mohashi-pro.local>
2020-11-27 16:29:46 -0300

    Fixing bug in the policy

--- policies/guid/0.1/guid.lua
+++ policies/guid/0.1/guid.lua
@@ -12,7 +12,7 @@ function _M.new(config)
 end
 
 function _M:rewrite()
-  local header_name = config.header_name or "GUID"
+  local header_name = self.header_name or "GUID"
 
   set_request_header(header_name, "3f596908-6822-402a-bf3c-e1679f188d56")
 end
