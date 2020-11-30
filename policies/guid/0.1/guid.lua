382253ce1c44d6876f76c3cd288764e4498999a7
Marcelo Ohashi <mohashi@mohashi-pro.local>
2020-11-30 10:56:11 -0300

    reordering functions

--- policies/guid/0.1/guid.lua
+++ policies/guid/0.1/guid.lua
@@ -4,6 +4,14 @@ local _M = policy.new('GUID policy', '0.1')
 local new = _M.new
 local random = math.random
 
+local function uuid()
+  local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+  return string.gsub(template, '[xy]', function (c)
+      local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+      return string.format('%x', v)
+  end)
+end
+
 function _M.new(config)
   local self = new(config)
 
@@ -16,14 +24,6 @@ function _M:rewrite()
   local header_name = self.header_name or 'GUID'
 
   ngx.req.set_header(header_name, uuid())
-end
-
-local function uuid()
-    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-    return string.gsub(template, '[xy]', function (c)
-        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
-        return string.format('%x', v)
-    end)
 end
 
 return _M
